86aa131c8d4d2e8067dc5e23a460777dd9ba330f
Bryan Brkic <bryan@MacBook-Pro.local>
2020-05-08 12:39:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# realest-estate
---
+++ build/precache-manifest.d105e252f6301eaa083f6b6ab80353c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97c0191353fbc07b311a1dcf81271bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64b2f0690f4c8fc5483",
+    "url": "/static/css/main.0a300f5e.chunk.css"
+  },
+  {
+    "revision": "7245d83c0cf95e07763d",
+    "url": "/static/js/2.cda887e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cda887e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64b2f0690f4c8fc5483",
+    "url": "/static/js/main.9eb076ec.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "54ad755d0ac2578303b874b92a394958",
+    "url": "/static/media/house.54ad755d.jpg"
+  }
+]);
